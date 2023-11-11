0fbc0b26e4b29bfef27e4bf063b827a026115350
jasaiful <jasaiful@gmail.com>
2023-11-11 13:48:49 +0600

    added bids section

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoAWSError, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -15,32 +15,111 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const userCollection = client.db('careerCenterDB').collection('user');
+        const jobsCollection = client.db('careerCenterDB').collection('jobs');
+        const myBids = client.db('careerCenterDB').collection('bids');
+        const googleUserCollection = client.db('careerCenterDB').collection('googleUser');
+
+        app.post("/myBids", async (req, res) => {
+            const data = req.body;
+            const result = myBids.insertOne(data);
+            res.send(result);
+        });
+
+        app.get("/myBids", async (req, res) => {
+            const result = await myBids.find().toArray();
+            res.send(result);
+        })
+        app.post('/jobs', async (req, res) => {
+            const job = req.body;
+            console.log(job);
+            const result = await jobsCollection.insertOne(job);
+            res.send(result);
+        })
+        app.get('/category/:id', async (req, res) => {
+            const path = req.params.id;
+            const result = await jobsCollection.find({ category: path }).toArray();
+            res.send(result.reverse());
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const path = req.params.id;
+            const objectId = new ObjectId(path);
+            const result = await jobsCollection.findOne({ _id: objectId });
+            res.send(result);
+        })
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+
+        });
 
 
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const job = await cursor.toArray();
+            res.send(job);
+        });
 
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
+        app.post('/googleUser', async (req, res) => {
+            const googleUser = req.body;
+            console.log(googleUser);
+            const result = await googleUserCollection.insertOne(googleUser);
+            res.send(result);
+        });
 
+        app.get('/googleUser', async (req, res) => {
+            const cursor = googleUserCollection.find();
+            const googleUser = await cursor.toArray();
+            res.send(googleUser);
+        });
 
+        app.get('/user', async (req, res) => {
+            const cursor = userCollection.find();
+            const user = await cursor.toArray();
+            res.send(user);
+        });
 
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.patch('/user', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const updateDoc = {
+                $set: {
+                    lastLoggedAt: user.lastLoggedAt
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
